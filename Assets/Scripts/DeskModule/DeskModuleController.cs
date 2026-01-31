97a8d99270c2bad953972ab480166a7ebc2836e3
Dorukhan Doruk <dorukhandoruk@panteon.games>
2026-01-31 22:36:14 +0300

    Update: desk module added

---
+++ Assets/Scripts/DeskModule/DeskModuleController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+namespace DeskModule
+{
+    public class DeskModuleController : MonoBehaviour
+    {
+        
+        
+        public void EnableDeskModule()
+        {
+        }
+        
+        public void DisableDeskModule()
+        {
+        }
+    }
+}
