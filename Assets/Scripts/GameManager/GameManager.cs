24ff852f47971c0018b37f422327f9e343c6a672
Bartu Baş <bartubas99@gmail.com>
2026-01-31 21:14:08 +0300

    Add initial game scene

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,10 +1,26 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameManager
 {
+	public enum GameState
+	{
+		WaitingForStartInput,
+		Incoming,
+		MaskSetup,
+		Outgoing,
+	}
+	
 	public class GameManager : MonoBehaviour
 	{
+		public GameState State;
 		
+		private void Start()
+		{
+			State = GameState.WaitingForStartInput;
+		}
 		private void Update()
 		{
 			if (Input.GetKeyDown(KeyCode.R))
