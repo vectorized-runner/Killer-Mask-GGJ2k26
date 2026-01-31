bb88c9976342a29739af563fbb827eb16cd42626
Bartu Baş <bartubas99@gmail.com>
2026-01-31 21:12:31 +0300

    R to restart scene

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,5 +5,12 @@ namespace GameManager
 	public class GameManager : MonoBehaviour
 	{
 		
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				SceneManager.LoadScene("Game");
+			}
+		}
 	}
 }
