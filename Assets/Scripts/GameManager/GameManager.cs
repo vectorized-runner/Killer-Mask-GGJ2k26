bfa175cee9f2f321a776cd7e5dcc328fdecc234d
Bartu Baş <bartubas99@gmail.com>
2026-01-31 21:14:16 +0300

    Add initial coroutine

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -17,16 +17,29 @@ namespace GameManager
 	{
 		public GameState State;
 		
+		private FreelookCamera _freelookCam;
+		
 		private void Start()
 		{
 			State = GameState.WaitingForStartInput;
+			
+			_freelookCam = FindFirstObjectByType<FreelookCamera>();
+			StartCoroutine(GameLoop());
 		}
+
 		private void Update()
 		{
 			if (Input.GetKeyDown(KeyCode.R))
 			{
 				SceneManager.LoadScene("Game");
 			}
+		}
+
+		private IEnumerator GameLoop()
+		{
+			_freelookCam.enabled = true;
+			
+			yield return null;
 		}
 	}
 }
